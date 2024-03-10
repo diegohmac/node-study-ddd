d736d02c070fc2852340f6d0003e68ca019d4436
Diego Machado <machadodiegoh@gmail.com>
2024-03-10 19:04:33 +0000

    refactor: add path alias

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,10 @@
-import { Slug } from './value-objects/slug';
-import { Entity } from '../../core/entities/entity';
-import { UniqueEntityID } from '../../core/entities/unique-entity-id';
-import { Optional } from '../../core/types/optional';
 import dayjs from 'dayjs';
 
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Entity } from '@/core/entities/entity';
+import { Optional } from '@/core/types/optional';
+
+import { Slug } from './value-objects/slug';
 interface QuestionProps {
   authorId: UniqueEntityID;
   bestAnswerId?: UniqueEntityID;
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true, // allow to use expect, test etc. without importing them
+  },
+});
