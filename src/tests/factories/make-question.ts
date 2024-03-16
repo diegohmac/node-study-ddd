17dcff0672e7ed355789a4d098f9e01b400d4f7d
Diego Machado <machadodiegoh@gmail.com>
2024-03-16 18:09:30 +0000

    test: add faker

--- src/tests/factories/make-question.ts
+++ src/tests/factories/make-question.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import {
   Question,
@@ -5,14 +6,20 @@ import {
 } from '@/domain/forum/enterprise/entities/question';
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    title: 'New Title',
-    content: 'New Content',
-    authorId: new UniqueEntityID('1'),
-    slug: Slug.create('new-title'),
-    ...override,
-  });
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID
+) {
+  const question = Question.create(
+    {
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      authorId: new UniqueEntityID('1'),
+      slug: Slug.create('new-title'),
+      ...override,
+    },
+    id
+  );
 
   return question;
 }
