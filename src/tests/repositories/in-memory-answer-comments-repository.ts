16f7631d99c713d297cdef24b9567a34ca4eb123
Diego Machado <machadodiegoh@gmail.com>
2024-03-17 12:03:14 +0000

    feat: add fetch-answer-comments use case

--- src/tests/repositories/in-memory-answer-comments-repository.ts
+++ src/tests/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
 
@@ -11,6 +12,15 @@ export class InMemoryAnswerCommentsRepository
       this.items.find((answerComment) => answerComment.id.toString() === id) ||
       null
     );
+  }
+
+  async findManyByAnswerId(answerId: string, params: PaginationParams) {
+    const { page } = params;
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answerComments;
   }
 
   async create(answerComment: AnswerComment) {
